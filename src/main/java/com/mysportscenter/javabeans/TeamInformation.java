c1f279077493a465a81864ed5bb9de3fd5f7e9aa
Jason <ozil.liu2@gmail.com>
2017-03-30 22:25:42 +0800

    Code change commit

---
+++ src/main/java/com/mysportscenter/javabeans/TeamInformation.java
@@ -0,0 +1,142 @@
+package com.mysportscenter.javabeans;
+
+/**
+ * Created by Jason on 3/30/17.
+ */
+public class TeamInformation {
+    private String id;
+    private int version;
+    private String teamName;
+    private String round;
+    private String winsCount;
+    private String drawCount;
+    private String loseCount;
+    private String points;
+    private String teamLogoPath;
+    private String updateTime;
+    private String custom1;
+    private String custom2;
+    private String custom3;
+    private String custom4;
+    private String custom5;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getRound() {
+        return round;
+    }
+
+    public void setRound(String round) {
+        this.round = round;
+    }
+
+    public String getWinsCount() {
+        return winsCount;
+    }
+
+    public void setWinsCount(String winsCount) {
+        this.winsCount = winsCount;
+    }
+
+    public String getDrawCount() {
+        return drawCount;
+    }
+
+    public void setDrawCount(String drawCount) {
+        this.drawCount = drawCount;
+    }
+
+    public String getLoseCount() {
+        return loseCount;
+    }
+
+    public void setLoseCount(String loseCount) {
+        this.loseCount = loseCount;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public String getTeamLogoPath() {
+        return teamLogoPath;
+    }
+
+    public void setTeamLogoPath(String teamLogoPath) {
+        this.teamLogoPath = teamLogoPath;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCustom1() {
+        return custom1;
+    }
+
+    public void setCustom1(String custom1) {
+        this.custom1 = custom1;
+    }
+
+    public String getCustom2() {
+        return custom2;
+    }
+
+    public void setCustom2(String custom2) {
+        this.custom2 = custom2;
+    }
+
+    public String getCustom3() {
+        return custom3;
+    }
+
+    public void setCustom3(String custom3) {
+        this.custom3 = custom3;
+    }
+
+    public String getCustom4() {
+        return custom4;
+    }
+
+    public void setCustom4(String custom4) {
+        this.custom4 = custom4;
+    }
+
+    public String getCustom5() {
+        return custom5;
+    }
+
+    public void setCustom5(String custom5) {
+        this.custom5 = custom5;
+    }
+}
